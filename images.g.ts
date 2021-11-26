cc553ca56ddf898e9838ffb58b59978c8e78183e
VadimOVB <64534501+VadimOVB@users.noreply.github.com>
2021-11-26 09:44:11 +0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage0":return img`
+. . . . . b c c . . . . . 
+. . . . . b c c . . . . . 
+b b b b . b c c . b b b b 
+1 1 1 1 . b c c . 1 1 1 1 
+b b b b c b c c c b b b b 
+1 1 1 1 c c c c c 1 1 1 1 
+b b b b e e e e e b b b b 
+1 1 1 1 e b 3 3 e 1 1 1 1 
+b b b b e b 3 3 e b b b b 
+1 1 1 1 e b b b e 1 1 1 1 
+b b b b e e e e e b b b b 
+1 1 1 1 c c c c c 1 1 1 1 
+b b b b . c c c . b b b b 
+1 1 1 1 . . . . . 1 1 1 1 
+`;
+            case "image1":
+            case "myImage":return img`
+. . . . 4 5 5 . . . . 
+. . . . 4 5 5 . . . . 
+5 5 5 . 4 5 5 . 5 5 5 
+4 4 4 . 4 5 5 . 4 4 4 
+5 5 5 5 5 5 5 3 5 5 5 
+4 4 4 4 4 4 4 4 4 4 4 
+5 5 5 4 5 2 2 4 5 5 5 
+4 4 4 4 5 5 2 4 4 4 4 
+5 5 5 4 5 5 5 4 5 5 5 
+4 4 4 4 4 4 4 4 4 4 4 
+5 5 5 5 5 5 5 5 5 5 5 
+4 4 4 . 4 5 5 . 4 4 4 
+5 5 5 . . . . . 5 5 5 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
